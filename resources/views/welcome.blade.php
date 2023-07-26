60f20e093a98eab707e04ae658a24c8d181f9f90
warmkatahira <t.katahira@warm.co.jp>
2023-07-26 10:13:25 +0900

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>smooth+</title>
+
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- Styles -->
+        @vite(['resources/css/app.css', 'resources/sass/theme.scss'])
+        <!-- Scripts -->
+        @vite(['resources/js/app.js'])
+        
+
+    </head>
+    <body>
+        
+    </body>
+</html>
