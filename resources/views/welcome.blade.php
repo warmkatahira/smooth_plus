aa58f7e791f53c28543b859bb282d960d315752a
warmkatahira <t.katahira@warm.co.jp>
2023-07-26 11:03:17 +0900

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,25 @@
 
         <!-- jQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/sass/theme.scss'])
-        <!-- Scripts -->
-        @vite(['resources/js/app.js'])
-        
 
+        <!-- favicon -->
+        <link rel="shortcut icon" href="{{ asset('image/favicon.svg') }}">
+
+        <!-- Styles -->
+        @vite(['resources/css/app.css', 'resources/scss/theme.scss', 'resources/css/welcome.css'])
+
+        <!-- LINE AWESOME -->
+        <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Alkatra&family=Kosugi+Maru&family=Lemonada&display=swap" rel="stylesheet">
+
+        <!-- Lordicon -->
+        <script src="https://cdn.lordicon.com/pzdvqjsp.js"></script>
+
+        <!-- toastr.js -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     </head>
     <body>
         
--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         laravel([
             'resources/css/app.css',
 			'resources/js/app.js',
+            'resources/scss/theme.scss',
         ]),
     ],
 });
