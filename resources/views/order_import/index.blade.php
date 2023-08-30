7538d6eac6b255e68f90bed83dcdc4706fa7b76f
warmkatahira <t.katahira@warm.co.jp>
2023-08-30 11:27:07 +0900

    20230830_1

--- resources/views/order_import/index.blade.php
+++ resources/views/order_import/index.blade.php
@@ -8,17 +8,6 @@
     <form method="POST" action="{{ route('order_import.import') }}" id="order_import_form" enctype="multipart/form-data" class="ml-5">
         @csrf
         <p class="border-l-4 border-theme-main pl-2 mb-2">受注データ選択</p>
-        {{-- <div class="flex flex-col mb-5">
-            <div class="flex select_file">
-                <p class="text-sm whitespace-nowrap select_file_name pl-5 py-1.5 bg-white overflow-hidden" style="width: 500px"></p>
-                <label class="text-sm whitespace-nowrap bg-gray-600 text-white inline-block text-center px-10 py-1.5 hover:cursor-pointer">
-                    <i class="las la-file-alt la-lg"></i>データ選択
-                    <input type="file" id="select_file" name="order_data" class="hidden">
-                </label>
-            </div>
-        </div> --}}
-
-
         <div id="select_file_div" class="bg-white border border-dashed border-gray-500 relative mb-2">
             <input type="file" id="select_file" name="order_data" class="cursor-pointer block absolute inset-0 w-full h-full opacity-0 z-50">
             <div class="text-sm text-center py-5">
@@ -26,7 +15,6 @@
             </div>
         </div>
         <p id="select_file_name" class="text-sm mb-3"></p>
-
         <div class="">
             <p class="border-l-4 border-theme-main pl-2 mb-2">受注インポート設定選択</p>
             <table class="text-xs whitespace-nowrap">
